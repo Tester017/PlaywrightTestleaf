6fba871b98b0b96dd81b17fba8764f3f5e3f9e29
tester017 <automationtester017@gmail.com>
2023-10-11 20:49:43 +0530

    first playwright

--- tests/firstplaywright.spec.ts
+++ tests/firstplaywright.spec.ts
@@ -3,6 +3,12 @@ import {chromium,test} from "@playwright/test"
 test("first test",async ()=>{
 
 const browser = await chromium.launch({headless:false})
- 
+const bContext = await browser.newContext();
+const bPage = await bContext.newPage();
+
+await bPage.goto("https://www.google.com/")
+const url = bPage.url
+console.log(`${bPage.url()}`)
+
 
 })
